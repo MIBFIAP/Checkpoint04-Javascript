37311f39e44a339e93a01db1581b589b5a93eaf9
MIB <mibfiap@outlook.com>
2023-09-11 22:53:36 -0300

    RM99215

--- src/Produtos.jsx
+++ src/Produtos.jsx
@@ -1,18 +1,31 @@
 import {} from 'react'
+import './App.css'; 
 
-function Produtos(){
-  return(
-    <>
-     <h1>Produtos</h1>
-     <h2>Iphone</h2>
-     <img src="../img/celular1.jfif" ></img>
-     <h2>Samsung</h2>
-     <img src="../img/celular2.jfif" ></img>
-     <h2>Huawey</h2>
-     <img src="../img/celular3.webp" ></img>
-     <h2>Multilaser</h2>
-     <img src="../img/celular4.webp" ></img>
-    </>
-  )
+function Produtos() {
+  return (
+    <div className="produtos-container">
+      <h1 className="produtos-title">Produtos</h1>
+      <div className="produtos-item">
+        <h2>Iphone</h2>
+        <img src="../img/celular1.jfif" alt="Iphone" />
+      </div>
+      <div className="produtos-item">
+        <h2>Samsung</h2>
+        <img src="../img/celular2.jfif" alt="Samsung" />
+      </div>
+      <div className="produtos-item">
+        <h2>Huawei</h2>
+        <img src="../img/celular3.webp" alt="Huawei" />
+      </div>
+      <div className="produtos-item">
+        <h2>Multilaser</h2>
+        <img src="../img/celular4.webp" alt="Multilaser" />
+      </div>
+    </div>
+  );
 }
-export default Produtos
+
+export default Produtos;
+
+
+
